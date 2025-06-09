a5be3b323822ee6e7cad5ff6679811d250665dd4
Jord <Jord@LAPTOP-VBUJ4N9D>
2025-06-09 20:53:19 +0100

    rebase

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"A87PIUQjJvsjqYwoqEz50GSBAYIDnCsCKfQCmo5Z8Z8=\"\n}"
---
+++ .next/static/chunks/app_page_tsx_ea6e06e0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__90b0960a._.css",
+  "static/chunks/f07ee_next_dist_bc353347._.js"
+],
+    source: "dynamic"
+});
